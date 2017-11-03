6f5ada5eb4a0bcc54a3401aeb3655b79fe9ea34d
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2017-11-02 23:16:22 -0300

    primera historia de usuario lista

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
 
+
+
+
+# devise_for :users
+devise_for :users, controllers: {
+        registrations: 'users/registrations'
+	} 
+
+#devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
+
  resources :menus
  get 'cooks/index'
  root to: 'cooks#index'
- devise_for :users
+
+ 
+
 
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
